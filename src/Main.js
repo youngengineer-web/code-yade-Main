44dc856e43a9877410a302eb1adb7b87d9c8662d
youngengineer <youngengineer.web@gmail.com>
2022-04-19 11:29:33 +0430

    section 12 - count - part1 => normaly style

--- src/Main.js
+++ src/Main.js
@@ -1,43 +1,44 @@
 import React from 'react';
+import Button from './Button';
 // import CountDown from './CountDoen';
 import './style.css'
 
-var getNumber = parseInt(prompt("Enter your Number: "));
 var interval;
 
 class Main extends React.Component {
     constructor() {
         super();
         this.state = {
-            countDown: getNumber
+            countDown: 0
         }
     }
 
     startInterval = () => {
         interval = setInterval(() => {
             this.setState({
-                countDown: this.state.countDown - 1
+                countDown: this.state.countDown + 1
             })
         }, 1000);
     }
 
-    componentDidMount() {
-        this.startInterval()
+    stopInterval = () => {
+        clearInterval(interval);
     }
 
-    componentDidUpdate() {
-        if (this.state.countDown == 0) {
-            clearInterval(interval);
-            document.getElementById('count').style.background='red';
-        }
+    // componentDidUpdate() {
+    //     if (this.state.countDown == 0) {
+    //         clearInterval(interval);
+    //         document.getElementById('count').style.background='red';
+    //     }
 
-    }
+    // }
 
     render() {
         return (
             // <CountDown countDown= {this.state.countDown} />
-            <div id='count'>
-                {this.state.countDown}
+            <div>
+                <p id='count'>{this.state.countDown}</p>
+                <Button startInterval={this.startInterval} stopInterval={this.stopInterval} />
             </div>
         )
     }
